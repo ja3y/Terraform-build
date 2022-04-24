35b5a9bb32ba83533ff2f0463f575c0ddf9f845a
ja3y <josephikeh1@gmail.com>
2022-04-24 18:29:22 +0100

    Terraform-install update

---
+++ Terraform-install.py
@@ -0,0 +1,9 @@
+import os
+#Repository configuration
+os.system("curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -")
+os.system("sudo apt-add-repository 'deb [arch=$(dpkg --print-architecture)] https://apt.releases.hashicorp.com $(lsb_release -cs) main'")
+#install terraform using apt package manager
+os.system("sudo apt install terraform")
+
+#Check terraform version
+os.system("terraform -v ")
